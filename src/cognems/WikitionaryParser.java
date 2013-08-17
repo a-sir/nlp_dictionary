75388b63193961d12da9d7170dc596167704a739
a-sir <alexander.sirenko@gmail.com>
2013-08-18 06:36:01 +0700

    Optimize imports.

--- src/cognems/WikitionaryParser.java
+++ src/cognems/WikitionaryParser.java
@@ -13,7 +13,6 @@ import javax.xml.parsers.SAXParserFactory;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
