3de766432999db9803fe2d2cc4c081e8f2950674
a-sir <alexander.sirenko@gmail.com>
2013-08-18 04:51:16 +0700

    Cognem with context/areas

---
+++ src/cognems/Cognem.java
@@ -0,0 +1,48 @@
+package cognems;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author A.Sirenko
+ * Date: 8/4/13
+ */
+public class Cognem {
+
+	public final String name;
+	public final String sense;
+	public final List<String> context;
+
+	public Cognem(@NotNull String name, @NotNull String sense, @NotNull String[] context) {
+		this.name = name;
+		this.sense = sense;
+		this.context = Collections.unmodifiableList(Arrays.asList(context));
+	}
+
+	public static class CognemBuilder {
+		public final String name;
+		public String sense;
+		public String[] context;
+
+
+		public CognemBuilder(@NotNull String name) {
+			this.name = name;
+		}
+
+		public CognemBuilder setAttributes(@NotNull String sense, @NotNull String ... context) {
+			this.sense = sense;
+			this.context = new String[context.length];
+			System.arraycopy(context, 0, this.context, 0, context.length);
+			return this;
+		}
+
+		@NotNull
+		public Cognem build() {
+			return new Cognem(name, sense, context);
+		}
+	}
+}
